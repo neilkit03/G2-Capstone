251b35cb2b75261ad73586a7e14f08d042f7b653
neilkit03 <neilkit03@gmail.com>
2023-03-24 14:24:38 +0800

    added server

---
+++ client/src/components/Portfolio.jsx
@@ -0,0 +1,210 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <>
+      <div id='portfolio'>
+        <div class='container'>
+          <div class='section-title'>
+            <h2>Our Works</h2>
+          </div>
+          <div class='row'>
+            <div class='portfolio-items'>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/01-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Two Storey House</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/01-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/02-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Kitchenette</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/02-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/03-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Galley</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/03-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/04-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Canopy</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/04-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/05-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Parkade & Entrance</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/05-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/06-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Single-family detached home</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/06-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/07-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Bathroom</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/07-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/08-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Scullery</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/08-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+              <div class='col-sm-6 col-md-4 col-lg-4'>
+                <div class='portfolio-item'>
+                  <div class='hover-bg'>
+                    {" "}
+                    <a
+                      href='img/portfolio/09-large.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div class='hover-text'>
+                        <h4>Cockloft</h4>
+                      </div>
+                      <img
+                        src='img/portfolio/09-small.jpg'
+                        class='img-responsive'
+                        alt='Project Title'
+                      />{" "}
+                    </a>{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Portfolio;
